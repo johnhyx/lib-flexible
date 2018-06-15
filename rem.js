e7f2492768e1de38c63af7023a6dbe958b4a5b8e
john_hyx <john_hyx@163.com>
2018-06-15 17:08:25 +0800

    lib

--- rem.js
+++ rem.js
@@ -1,4 +1,4 @@
-//在去深入了解淘宝flexible之前，首先先对一些基础概念做一些了解；
+//在去深入了解淘宝flexible之前，首先先对一些基础概念做了一些了解；
 
 //视窗Viewport
 /*
@@ -74,3 +74,134 @@
 *
 * 除此之外，在引入lib-flexible需要执行的js之前，可以手动设置meta来控制dpr的值
 */
+
+
+//flexible源码
+/*
+*使用立即函数表达式，避免全局作用域污染
+* 通过传入window对象，可以使window对象变成局部变量
+* 当在代码访问window对象是，就不需要将作用域退回到顶层作用域，这样可以更快的访问到window
+* 将window对象作为参数传入，还可以在代码压缩是进行优化
+*/
+;(function(win, lib) {
+    var doc = win.document;//document对象
+    var docEl = doc.documentElement;//文档根节点
+    var metaEl = doc.querySelector('meta[name="viewport"]');//获取name值为viewport的meta节点
+    var flexibleEl = doc.querySelector('meta[name="flexible"]');
+    var dpr = 0;//初始化dpr
+    var scale = 0;//初始化缩放比例
+    var tid;//定时器id
+    var flexible = lib.flexible || (lib.flexible = {});//将flexible对象暴露给全局作域，这样就可以直接在外部调用内部的一些方法和变量
+
+    if (metaEl) {//如果文档已经设置了‘viewport’
+        console.warn('将根据已有的meta标签来设置缩放比例');
+        var match = metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);//获取到‘content’的属性值，并用正则模式匹配
+        if (match) {//在‘content’属性设置了‘缩放比例’
+            scale = parseFloat(match[1]);//获取缩放比例
+            dpr = parseInt(1 / scale);//获取dpr
+        }
+    } else if (flexibleEl) {
+        var content = flexibleEl.getAttribute('content');
+        if (content) {
+            var initialDpr = content.match(/initial\-dpr=([\d\.]+)/);
+            var maximumDpr = content.match(/maximum\-dpr=([\d\.]+)/);
+            if (initialDpr) {
+                dpr = parseFloat(initialDpr[1]);
+                scale = parseFloat((1 / dpr).toFixed(2));
+            }
+            if (maximumDpr) {
+                dpr = parseFloat(maximumDpr[1]);
+                scale = parseFloat((1 / dpr).toFixed(2));
+            }
+        }
+    }
+
+    if (!dpr && !scale) {//dpr和缩放比例都没有设置
+        var isAndroid = win.navigator.appVersion.match(/android/gi);
+        var isIPhone = win.navigator.appVersion.match(/iphone/gi);
+        var devicePixelRatio = win.devicePixelRatio;
+        if (isIPhone) {//苹果设备
+            // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
+            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
+                dpr = 3;//scale = 0.3333333
+            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
+                dpr = 2;//scale = 0.5
+            } else {
+                dpr = 1;//scale = 1
+            }
+        } else {
+            // 其他设备下，仍旧使用1倍的方案
+            dpr = 1;
+        }
+        scale = 1 / dpr;//获取缩放比例
+    }
+
+    docEl.setAttribute('data-dpr', dpr);//在根节点设置'data-dpr'属性，这个属性相当重要，有这个属性我们就可以，设置不同缩放比例下的字体的大小，设置依然使用rem；
+    if (!metaEl) {//文档没有设置‘viewport’
+        metaEl = doc.createElement('meta');//创建一个‘meta’节点
+        metaEl.setAttribute('name', 'viewport');//设置‘meta’节点的name属性
+        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no')//设置meta节点的'content'属性;
+        if (docEl.firstElementChild) {//判断有没有元素，兼容
+            docEl.firstElementChild.appendChild(metaEl);
+        } else {
+            var wrap = doc.createElement('div');
+            wrap.appendChild(metaEl);
+            doc.write(wrap.innerHTML);
+        }
+    }
+    /*
+    * '普通屏幕'一个css像素 => 一个物理像素
+    * '视网膜屏'一个css像素 => 四个物理像素
+    */
+    function refreshRem(){
+        var width = docEl.getBoundingClientRect().width;//获取设备上document元素的宽度值，利用getBoundingClientRect()方法；
+        if (width / dpr > 540) {//'css'最高像素是540（是一个经验值）;
+            width = 540 * dpr;//设置document的width
+        }
+        var rem = width / 10//计算出基准像素
+        docEl.style.fontSize = rem + 'px';//设置文档的基准像素
+        flexible.rem = win.rem = rem;//定义了一个全局变量，可以通过window.rem或者lib.flexible.rem来获取基准像素
+
+    }
+
+    win.addEventListener('resize', function() {//监听视图窗口发生变化
+        clearTimeout(tid);//清空计时器，防止再次触发refreshRem函数
+        tid = setTimeout(refreshRem, 300);//延迟300毫秒，重新计算并设置html的'font-size'
+    }, false);
+    win.addEventListener('pageshow', function(e) {/*重载页面时候触发，这里监听pagashow事件没有监听load事件的原因是，在opera和火狐浏览器中，pageshow事件在后退/前进这两个动作是会触发，
+                                                     但是load事件因为页面已经被加载过了，被存到缓存里面就不会触发了，所以我们选择监听pageshow事件。*/
+        if (e.persisted) {//从缓存中加载页面(浏览器自带后退功能)
+            clearTimeout(tid);//清空计时器，防止再次触发refreshRem函数
+            tid = setTimeout(refreshRem, 300);//延迟300毫秒，重新计算并设置html的'font-size'
+        }
+    }, false);
+
+    if (doc.readyState === 'complete') {
+        doc.body.style.fontSize = 12 * dpr + 'px';
+    } else {
+        doc.addEventListener('DOMContentLoaded', function(e) {//DOM构建完成后触发
+            doc.body.style.fontSize = 12 * dpr + 'px';//设置body的默认字体大小
+        }, false);
+    }
+
+
+    refreshRem();
+
+    flexible.dpr = win.dpr = dpr;
+    flexible.refreshRem = refreshRem;
+    flexible.rem2px = function(d) {//将rem转换为px
+        var val = parseFloat(d) * this.rem;//this.rem是基准像素
+        if (typeof d === 'string' && d.match(/rem$/)) {
+            val += 'px';//将值转换为字符串，并带上'px'单位
+        }
+        return val;
+    }
+    flexible.px2rem = function(d) {//将px转换为rem
+        var val = parseFloat(d) / this.rem;//this.rem是基准像素
+        if (typeof d === 'string' && d.match(/px$/)) {
+            val += 'rem';//将值转换为字符串，并带上'rem'单位
+        }
+        return val;
+    }
+
+})(window, window['lib'] || (window['lib'] = {}));//window[ 'lib' ] = {} 等同于 var lib = {} 这里很隐晦的定义了一个全局变量'lib';
